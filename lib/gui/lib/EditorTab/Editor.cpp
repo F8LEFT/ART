a212eb5a3497dad912f96af1357c3491a25ecfc1
F8LEFT <826788949@qq.com>
2016-09-27 17:22:01 +0800

    add Editor

---
+++ lib/gui/lib/EditorTab/Editor.cpp
@@ -0,0 +1,148 @@
+//===- Editor.cpp - ART-GUI Editor Tab -------------------------*- C++ -*-===//
+//
+//                     ANDROID REVERSE TOOLKIT
+//
+// This file is distributed under the GNU GENERAL PUBLIC LICENSE
+// V3 License. See LICENSE.TXT for details.
+//
+//===---------------------------------------------------------------------===//
+#include "Editor.h"
+
+#include <utils/Configuration.h>
+
+#include <QTextStream>
+#include <QtConcurrent/QtConcurrent>
+
+void readFileThread(Editor* e, QString filePath);
+void saveFileThread(QString text, QString  filePath);
+
+Editor::Editor(QWidget *parent) :
+        QWidget(parent)
+{
+    mFileChangedTimer = new QTimer(this);
+    mFileEdit = allocTextEditorWidget ();
+
+    connect(this, SIGNAL(readLine(QString)), this, SLOT(appendLine(QString)));
+    connect(this, SIGNAL(readEnd()), this, SLOT(readFileEnd()));
+
+    connect(mFileChangedTimer, SIGNAL(timeout()), this, SLOT(textChangedTimeOut()));
+
+    QFont font;
+    font.setFamily("Courier");
+    font.setFixedPitch(true);
+    font.setPointSize(10);
+
+    mFileEdit->setFont(font);
+
+    loadFromConfig();
+}
+
+Editor::~Editor()
+{
+    closeFile();
+    saveToConfig();
+}
+
+bool Editor::openFile(QString filePath, QString fileName)
+{
+    fp = filePath;
+    fn = fileName;
+    QFile file(fp);
+    if (file.open(QFile::ReadOnly | QFile::Text)) {
+        mFileEdit->setReadOnly(true);
+        mFileEdit->setDocumentTitle(fileName);
+        mFileEdit->setDocumentPath(filePath);
+        file.close();
+
+        QtConcurrent::run(readFileThread, this, filePath);
+        return true;
+    }
+    return false;
+}
+
+bool Editor::saveFile()
+{
+    mFileChangedTimer->stop();
+    if (isUpdated) {
+        QtConcurrent::run(saveFileThread, mFileEdit->toPlainText(), fp);
+    }
+
+    isUpdated = false;
+}
+
+bool Editor::reload()
+{
+    mFileEdit->clear();
+    QtConcurrent::run(readFileThread, this, fp);
+}
+
+void Editor::closeFile()
+{
+    saveFile();
+}
+
+void Editor::loadFromConfig()
+{
+//    Configuration *config = Config();
+}
+
+void Editor::saveToConfig()
+{
+//    Configuration *config = Config();
+}
+
+void Editor::gotoLine(int line, int column, bool centerLine)
+{
+    mFileEdit->gotoLine(1, column, centerLine);
+}
+
+void Editor::appendLine(QString line)
+{
+    mFileEdit->appendPlainText(line);
+}
+
+void Editor::readFileEnd()
+{
+    mFileEdit->setReadOnly(false);
+    mFileEdit->setFocus();
+    connect(mFileEdit, SIGNAL(textChanged()), this, SLOT(textChanged()));
+    mFileEdit->gotoLine(1);
+}
+
+void Editor::textChanged()
+{
+    isUpdated = true;
+    mFileChangedTimer->start(60*1000);
+}
+
+void Editor::textChangedTimeOut()
+{
+    saveFile();
+}
+
+TextEditorWidget *Editor::allocTextEditorWidget ()
+{
+    return new TextEditorWidget(this);
+}
+
+void readFileThread(Editor* e, QString filePath) {
+    QFile file(filePath);
+    if (file.open(QFile::ReadOnly | QFile::Text)) {
+        while(!file.atEnd()) {
+            emit e->readLine(file.readAll());
+        }
+        file.close();
+    }
+    emit e->readEnd();
+    return ;
+}
+
+void saveFileThread(QString text, QString  filePath) {
+    QFile file(filePath);
+    if (file.open(QFile::WriteOnly | QFile::Truncate | QFile::Text)) {
+        QTextStream out(&file);
+        out<<text;
+        out.flush();
+        file.close();
+    }
+}
---
+++ lib/gui/lib/EditorTab/Editor.h
@@ -0,0 +1,67 @@
+//===- Editor.h - ART-GUI Editor Tab ---------------------------*- C++ -*-===//
+//
+//                     ANDROID REVERSE TOOLKIT
+//
+// This file is distributed under the GNU GENERAL PUBLIC LICENSE
+// V3 License. See LICENSE.TXT for details.
+//
+//===---------------------------------------------------------------------===//
+//
+// Editor is a widget to open/modify/save file.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef PROJECT_EDITOR_H
+#define PROJECT_EDITOR_H
+
+#include "TextEditorWidget.h"
+
+#include <QWidget>
+#include <QThread>
+#include <QTimer>
+
+class Editor : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit Editor(QWidget *parent = 0);
+    ~Editor();
+
+    bool openFile(QString filePath, QString fileName);
+    bool saveFile();
+    bool reload();
+    void closeFile();
+
+    QString getFilePath() {return fp;}
+    QString getFileName() {return fn;}
+
+    void loadFromConfig();
+    void saveToConfig();
+
+    void gotoLine(int line, int column = 0, bool centerLine = true);
+
+    virtual TextEditorWidget* allocTextEditorWidget();
+
+signals:
+    void readLine(QString line);
+    void readEnd();
+
+public slots:
+    void appendLine(QString line);
+    void readFileEnd();
+
+    void textChanged();
+    void textChangedTimeOut();
+
+protected:
+    QString fp;
+    QString fn;
+
+    bool isUpdated = false;
+    QTimer* mFileChangedTimer;
+    TextEditorWidget* mFileEdit;
+};
+
+
+#endif //PROJECT_EDITOR_H
